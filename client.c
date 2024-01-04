3f2dcadeadb68c5543bba1f3f2ecbaa02b96000a
Rachel Gao <rachelgao0421@gmail.com>
2024-01-04 14:37:48 -0500

    imported code from perious lab to work off of

--- client.c
+++ client.c
@@ -1,1 +1,38 @@
+#include "networking.h"
+#include "gameFunctions.h"
+
+void stripNewLine(char* input){
+  for(int x = 0; x < strlen(input); x++){
+    if (strcmp(&input[x], "\n") == 0 || strcmp(&input[x], "\r") == 0){
+      input[x] = 0; // removes new line
+      break;
+    }
+  }
+}
+
+void clientLogic(int server_socket){
+  char line[BUFFER_SIZE];
+  printf("enter your message:\n");
+  fgets(line, BUFFER_SIZE, stdin);
+  stripNewLine(line);
+  //printf("writing...\n");
+  int b = write(server_socket, line, BUFFER_SIZE);
+  if (b == -1) err(6, "client write broke");
+  //printf("reading...\n");
+  b = read(server_socket, line, BUFFER_SIZE);
+  if (b == -1) err(6, "client read broke");
+  printf("%s\n", line);
+}
+
+int main(int argc, char *argv[] ) {
+  char* IP = "127.0.0.1";
+  if(argc>1){
+    IP=argv[1];
+  }
+  int server_socket = client_tcp_handshake(IP);
+  printf("client connected.\n");
+  while(1){
+    clientLogic(server_socket);
+  }
+}
 
--- makefile
+++ makefile
@@ -1,1 +1,20 @@
+compile: client.o server.o networking.o
+	@gcc -o client client.o networking.o
+	@gcc -o server server.o networking.o
 
+client: compile
+	@./client $(ARGS)
+
+server: compile
+	@./server
+
+client.o: client.c networking.h
+	@gcc -c client.c networking.h
+
+server.o: server.c networking.h
+	@gcc -c server.c networking.h
+
+clean:
+	rm *.o
+	rm ./client
+	rm ./server
