92f96ea930276bf86db0369d79e4de52be544660
Ayuremi <rachelgao0421@gmail.com>
2024-01-04 21:48:48 -0500

    started implementing multiple clients but other clients doesnt process string

--- server.c
+++ server.c
@@ -27,29 +27,53 @@ void subserver_logic(int client_socket){
 int main(int argc, char *argv[] ) { 
 
     int listen_socket = server_setup();
+    int max_sd = listen_socket; // max socket descriptor, will change as clients connect
+    int MAX_CLIENTS = 10;
+    int clients[MAX_CLIENTS]; // store client socket file descriptors
+    for (int x = 0; x < MAX_CLIENTS; x++) clients[x] = 0; // initialize
     fd_set read_fds;
 
     char buff[1025]="";
 
     while (1){
-
+        
         FD_ZERO(&read_fds);
-        FD_SET(STDIN_FILENO, &read_fds);
         FD_SET(listen_socket,&read_fds);
-        int i = select(listen_socket+1, &read_fds, NULL, NULL, NULL);
+        for (int x = 0; x < MAX_CLIENTS; x++){
+            if (clients[x] > 0){
+                FD_SET(clients[x], &read_fds); // add clients to fds
+            }
+            if (clients[x] > max_sd) max_sd = clients[x]; // update max socket descriptor
+        }
+        int i = select(max_sd+1, &read_fds, NULL, NULL, NULL);
 
-        // if socket
+        // if listen_socket, accept connection and add to client sockets array
         if (FD_ISSET(listen_socket, &read_fds)) {
             //accept the connection
             int client_socket = server_tcp_handshake(listen_socket);
+            for (int x = 0; x < MAX_CLIENTS; x++){
+                if (clients[x] == 0) clients[x] = client_socket;
+                break; //to add once
+            }
             printf("Connected, waiting for data.\n");
 
-            //while(1){
-                subserver_logic(client_socket);
-            //}
-
+            //subserver_logic(client_socket);
+            
             //close(client_socket);
         }
+
+        //if existing client
+        for (int x = 0; x < MAX_CLIENTS; x++){
+            int sd = clients[x];
+            if (FD_ISSET(sd, &read_fds)){
+                subserver_logic(sd);
+            }
+
+        }
+
+
+
+
     }
 }
 
