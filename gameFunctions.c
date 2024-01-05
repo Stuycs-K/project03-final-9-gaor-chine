7b9fcd5cff6232c59e3ea18f5334825e928d4b92
Edmund Chin <edmundc421@gmail.com>
2024-01-05 14:51:16 -0500

    reading from standard out soon...

--- gameFunctions.c
+++ gameFunctions.c
@@ -14,7 +14,17 @@
 #include <fcntl.h>
 
 #define BUFFERSIZE 100
+#define READ 0
+// #define WRITE 1
+
 struct player{char name[BUFFERSIZE];int lives;};
+
+void err(int i, char*message){
+  if(i < 0){
+	  printf("Error: %s - %s\n",message, strerror(errno));
+  	exit(1);
+  }
+}
 
 void parse_args(char * line, char * args[]){
     char * linePointer = line;
@@ -32,29 +42,35 @@ returns 1 if word doesn't exist
 returns 0 of word exists
 */
 int parse(char word[]){
-    // int wordLen = strlen(word);
-    // char string[wordLen];
-    // strncpy(string,word,wordLen);
-    // printf("Trimmed string: %s\n",string);
-    // word[strlen(word)-1] = 0;
-    char front[] = "grep ";
-    strcat(front,word);
-    char back[] = " words_alpha.txt -w";
-    strcat(front,back);
-    printf("String: %s\n",front);
     char * cmdargv[64];
+    char front[100] = "grep ";
+    char back[] = " words_alpha.txt -w";
+
+    int usedWords = open("usedWords.txt" , O_CREAT|O_RDWR,644 );
+    // printf("String: %s\n",front);
+    strcat(front,word);
+    // printf("String: %s\n",front);
+    strcat(front,back);
+    // printf("String: %s\n",front);
     parse_args(front,cmdargv);
+    
+    char grepReturn[100];
     execvp(cmdargv[0],cmdargv);
+    // fgets(grepReturn, 100, stdout);
+    int sOut = open("/dev/stdout",100,O_RDONLY);
+    int i = read(sOut,grepReturn,100);
+    err(i, "fgets fails");
+    printf("returned val: %s\n", grepReturn);
     return 0;
 }
 //---------------------------------------------------------------------------------------------
 int main(int argc, char * argv[]){
     
     printf("Tests:\n");
-    char c[] = "yes";
+    char c[] = "apply";
     int i;
     i = parse(c);
-    printf("returned: %d\n", i);
+    // printf("returned: %d\n", i);
 }
 
 
--- gameFunctions.h
+++ gameFunctions.h
@@ -1,6 +1,7 @@
 #ifndef GAMEFUNCTIONS_H
 #define GAMEFUNCTIONS_H
 struct player{char name[100];int lives;};
+void err(int i, char*message);
 void parse_args(char * line, char * args[]);
 int parse(char word[]);
 #endif
