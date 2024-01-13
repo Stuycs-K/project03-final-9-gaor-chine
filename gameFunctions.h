2ad52a268e30a32b899e95b6132dcd59fcb06ec7
edmundc421 <edmundc421@gmail.com>
2024-01-13 08:41:35 -0500

    randPrompt finished

--- gameFunctions.h
+++ gameFunctions.h
@@ -1,6 +1,7 @@
 #ifndef GAMEFUNCTIONS_H
 #define GAMEFUNCTIONS_H
 struct player{char name[100];int lives;int sd;};
+char * randPrompt();
 struct player* create_player(char *name, int sd);
 void parse_args(char * line, char * args[]);
 int parse(char word[]);
